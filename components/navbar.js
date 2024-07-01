30360496ed85a3e0a4e20c58cc340240df15621f
AnB School 0026 <anbschool0026@AnBs-Mac-mini-157.local>
2024-07-01 21:46:52 +0700

    add component to navbar and footer

---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+const navbar = () => {
+    return 
+    `
+    <navbar id="navbar">
+        <h2>KuLiNote</h2>
+        <img src="../image/List-Bullets--Streamline-Ultimate 1.png" alt="">
+    </navbar>
+    `
+}
+
+class Navbar extends HTMLElement {
+    connectedCalledback () {
+        this.innerHTML = navbar();
+    }
+}
+
+customElements.define('navbar-element', Navbar)
