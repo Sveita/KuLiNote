d04d1653ec2c02fd8b274525a175d19feaa212d0
AnB School 0026 <anbschool0026@AnBs-Mac-mini-157.local>
2024-07-03 19:16:34 +0700

    feature : add delete feature

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { createCard } from "./components/card.js";
-
+import { deleteDiary } from "./scripts/delete.js";
 const display = ()=>{
     const cardContainer = document.querySelector("#card-container");
     const diaryArr = JSON.parse(localStorage.getItem('diaryArray'));
@@ -8,5 +8,12 @@ const display = ()=>{
         cardContainer.innerHTML += createCard(item.id,item.title, item.content , item.date);
     })
 }
-
 display();
+
+const btnDelete = document.querySelectorAll("#deleteNote");
+
+btnDelete.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        deleteDiary(e.target.id);
+    })
+})
