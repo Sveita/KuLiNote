3d1d6f62bc865da00591ba5028d231059a4983f5
AnB School 0026 <anbschool0026@AnBs-Mac-mini-157.local>
2024-07-03 16:16:29 +0700

    edit to css

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,5 @@
 const navbar = () => {
-    return 
-    `
-    <navbar id="navbar">
+    return `<navbar id="navbar">
         <h2>KuLiNote</h2>
         <img src="./image/List-Bullets--Streamline-Ultimate 1.png" alt="">
     </navbar>
@@ -9,9 +7,9 @@ const navbar = () => {
 }
 
 class Navbar extends HTMLElement {
-    connectedCalledback () {
-        this.innerHTML = navbar();
+    constructor(){
+        super()
+        this.innerHTML = navbar(); 
     }
 }
-
-customElements.define('navbar_element', Navbar)
+customElements.define('navbar-element', Navbar)
